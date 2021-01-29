da8fd4d9386d42521cbd8d17d8879853aa9b8d2f
Kazi Abdullah Al Mamun <dev07.iapps@gmail.com>
2021-01-29 11:58:31 +0600

    Initial Commit

---
+++ Treker/TrekerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrekerApp.swift
+//  Treker
+//
+//  Created by iapps on 29/1/21.
+//
+
+import SwiftUI
+
+@main
+struct TrekerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
